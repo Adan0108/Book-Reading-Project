c4c2c2a4b41bb36258d66fdd041f3dc0b9a3cb3f
JasmineTrinh <jasminetrinh1006@gmail.com>
2025-11-06 00:48:14 +1100

    revert the server.ts file back into its original version

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,9 @@
 import "dotenv/config";
 import app from "./app";
-import { runUserCleanup } from './services/cleanup.service'; // <-- Import the cleanup service
 
 const PORT = Number(process.env.PORT || 3000);
-const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 const server = app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
-
-  // Run the cleanup task immediately on startup...
-  runUserCleanup();
-  // ...and then run it again every 24 hours.
-  setInterval(runUserCleanup, ONE_DAY_IN_MS);
 });
 
 process.on("SIGINT", () => {
